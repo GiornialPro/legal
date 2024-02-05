098f8bb6f2c09edbe1d9bc05cd0616fd228927ec
GiornialPro <157518489+GiornialPro@users.noreply.github.com>
2024-02-05 16:38:39 +0100

    Update apph.py

--- apph.py
+++ apph.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+fonte = 'https://github.com/GiornialPro/legal/blob/main/'
+
 
 @app.route('/')
 def casa():
 
     user = 'Adaylson Dias, Eng.'
 
-    return render_template('home.html', user = user)
+    return render_template( f'{fonte}+home.html' , user = user)
 
 if __name__ == "__main__":
     app.run( port='10000', host = '0.0.0.0' )
