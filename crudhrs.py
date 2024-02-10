60f25bd7702bdc9dba1da818cf35504e570e8ae1
GiornialPro <157518489+GiornialPro@users.noreply.github.com>
2024-02-10 22:21:30 +0100

    Add files via upload

---
+++ crudhrs.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+from openpyxl import load_workbook, Workbook
+import xlrd
+
+
+class Sistema:
+
+    def __init__(self) -> None:
+        pass
+
+    def origina(self):
+        
+        pass
+        book = Workbook()
+
+        book.save('HORARIOS..xlsx')
