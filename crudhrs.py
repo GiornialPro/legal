3eb8d016236be58697ca5ac25e216c8cc4fbe9ee
GiornialPro <projadec1.service@gmail.com>
2024-02-12 17:51:53 +0100

    deleted:    home.html

--- crudhrs.py
+++ crudhrs.py
@@ -1,4 +1,5 @@
 import os
+from flask import request
 import pandas as pd
 from openpyxl import load_workbook, Workbook
 import xlrd
@@ -6,15 +7,20 @@ import xlrd
 
 class Sistema:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sourc):
+        self.sourc = 'UNIDADE.xlsx'
 
     def origina(self):
         
         pass
-        book = pd.read_excel('UNIDADE.xlsx', sheet_name='unidade')
+        book = pd.read_excel(self.sourc, sheet_name='unidade')
         book_dic = book.to_dict('list')
 
 
 
         return book_dic
+
+    def addciona(self):
+        
+        book_to_add = load_workbook(self.sourc)
+
