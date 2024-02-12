21dcdbc80c03fe76f4c9150e13f9d060af2d0b1e
GiornialPro <projadec1.service@gmail.com>
2024-02-12 23:21:28 +0100

    up: sht

--- crudhrs.py
+++ crudhrs.py
@@ -43,6 +43,7 @@ class Sistema:
             
             book_to_add.create_sheet('Membros')
 
+            
             book_to_add['unidade'][xlrd.cellname( 0, 0 )] = 'NOME'
             book_to_add['unidade'][xlrd.cellname( 0, 1 )] = 'CHAVE'
             book_to_add['unidade'][xlrd.cellname( 1, 0 )] = user
