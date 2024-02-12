3bb52f07851e19a9d511b39bb880b90a393fc5a4
GiornialPro <projadec1.service@gmail.com>
2024-02-12 21:10:36 +0100

    up: modfy

--- apph.py
+++ apph.py
@@ -10,12 +10,22 @@ app.config["SESSION_PERMANET"] = False
 @app.route('/', methods=['POST','GET'] )
 def casa():
 
+    user = 'Adaylson Dias, Eng.'
+    data = Sistema('UNIDADE.xlsx').origina()
+
+    return render_template( 'home.html' , user = user, data=data)
+
+
+@app.route('/home', methods=['POST','GET'] )
+def home():
+
     a = Sistema('UNIDADE.xlsx').addciona()
 
     user = 'Adaylson Dias, Eng.'
     data = Sistema('UNIDADE.xlsx').origina()
 
     return render_template( 'home.html' , user = user, data=data, a=a)
+
 
 if __name__ == "__main__":
     app.run( port='10000', host = '0.0.0.0' )
